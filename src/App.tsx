2bf3a07b168c3e1fc7bcd892cdf68bc909d50ab0
Alan Lai <dev.alan.au@gmail.com>
2024-11-29 23:23:06 +1100

    Add V2 message format support

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,32 @@ import React, { useState } from 'react';
 import './App.css'
 
 type Message = {
-    sender_name: string;
-    timestamp_ms: number;
-    content: string;
-    call_duration: number;
-    is_geoblocked_for_viewer: boolean;
-    reactions?: any[];
-    photos?: any[];
-    gifs?: any[];
-    share?: undefined;
-    videos?: any[];
-    is_unsent?: undefined;
-    sticker?: undefined;
+  sender_name: string;
+  timestamp_ms: number;
+  content: string;
+  call_duration: number;
+  is_geoblocked_for_viewer: boolean;
+  reactions?: any[];
+  photos?: any[];
+  gifs?: any[];
+  share?: undefined;
+  videos?: any[];
+  is_unsent?: undefined;
+  sticker?: undefined;
 };
 
+type MessageV2 = {
+  isUnsent: boolean,
+  media: any[],
+  reactions: {actor:string; reaction: string}[],
+  senderName: string,
+  text: string,
+  timestamp: number,
+  type: "text" | "media"
+}
+
 function App() {
-  const [messages, setMessages] = useState<Message[]>()
+  const [messages, setMessages] = useState<Message[]|MessageV2[]>()
   const updateData = async (ev: React.ChangeEvent<HTMLInputElement>) => {
     const files = ev.currentTarget.files;
     if(files?.length) {
@@ -38,7 +48,12 @@ function App() {
       <input type="file" onChange={updateData} />
       <div>
         {messages?.map((message, idx) => {
-          return <Message message={message} key={idx} />
+          if('sender_name' in message) {
+            return <Message message={message} key={idx} />
+          }
+          if('senderName' in message) {
+            return <MessageV2 message={message} key={idx} />
+          }
         })}
       </div>
     </div>
@@ -46,8 +61,6 @@ function App() {
 }
 
 export default App
-
-
 const Message = ({message} : {message: Message}) => {
   const date = new Date(message.timestamp_ms);
   const year = date.getFullYear();
@@ -70,6 +83,47 @@ const Message = ({message} : {message: Message}) => {
             <div>{message.videos?.length && '🎥'}</div>
           </div>
         }
+      </div>
+      <br />
+      <br />
+    </div>
+  )
+}
+
+const MessageV2 = ({message} : {message: MessageV2}) => {
+  const date = new Date(message.timestamp);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
+  return (
+    <div>
+      <div><strong>Sent: {message.senderName}</strong></div>
+      <div><small>Date: {`${year}.${month < 10 ? '0' + month : month}.${day < 10 ? '0' + day : day}`}</small></div>
+      <div>
+        <div><strong>Type: {message.type}</strong></div>
+        {
+          message.type === 'text' && (
+            <div>
+              <div>{message.text}</div>
+            </div>
+          )
+        }
+        {
+          message.type === 'media' && (
+            <div>🎬</div>
+          )
+        }
+        {message.reactions.length > 0 && (
+          <div>
+            <strong>Reactions:</strong>
+            <div>
+              {message.reactions.map((reaction, idx) => {
+                return <div key={idx}>{reaction.reaction}</div>
+              })}
+            </div>
+          </div>
+        )}
       </div>
       <br />
       <br />
